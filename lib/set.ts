6aa441d9571c26579e49ec52413b69d038516823
Nathan <nathan@berkeley.edu>
2016-09-17 08:45:16 +0100

    because semicolons matter

--- lib/set.ts
+++ lib/set.ts
@@ -18,12 +18,12 @@ let newDeck = ['01',
 '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', 
 '52', '53', '54', '55', '56', '57', '58', '59', '60', '61', 
 '62', '63', '64', '65', '66', '67', '68', '69', '70', '71', 
-'72', '73', '74', '75', '76', '77', '78', '79', '80', '81']
+'72', '73', '74', '75', '76', '77', '78', '79', '80', '81'];
 
 if (Meteor.isServer) {
     Meteor.publish('gamestate', function(room: string) {
         return SetGameStateCollection.find({room: room}, {fields: {cards: 1, score: 1}});
-    })
+    });
 }
 
 Meteor.methods({
@@ -88,4 +88,4 @@ Meteor.methods({
             SetGameStateCollection.update({_id: state._id}, state);
         }
     }
-})
+});
